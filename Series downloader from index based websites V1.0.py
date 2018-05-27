e855e4d379e5b658dadb37186ca548a3ba0fa2d9
Apoorv Rusia <apoorvrusia@gmail.com>
2018-05-27 16:07:06 -0400

    Update Series downloader from index based websites V1.0.py

--- Series downloader from index based websites V1.0.py
+++ Series downloader from index based websites V1.0.py
@@ -1,12 +1,5 @@
-
-# coding: utf-8
-
 # Importing the neccessary packages for web scraping the videos
 
-# In[96]:
-
-
-#importing the packages we need for scraping
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
@@ -15,58 +8,33 @@ import urllib.request
 
 
 # Using input box to enter the url of the website
-
-# In[99]:
-
-
 #adding the url name
 url = input('Enter the url: ')
 #url = 'http://fromv.ir/vip/Up/Animation%20Series/Ultimate%20Spiderman/'
 
-
 # Using input box to ask the user what name has to be used for the videos for saving the file
-
-# In[100]:
-
-
 #asking for series name for giving the name to the file
 seriesName = input('Enter the series name: ')
 print(seriesName)
 
 
 # Checking whether the request get any response from the server or not.
-
-# In[101]:
-
-
 #page is a requests object which holds the downloaded html page
 page = requests.get(url)
 print(page)
 
 
 # Creating the BeautifulSoup object for parsing the html
-
-# In[102]:
-
-
 #creating the BeautifulSoup object
 soup = BeautifulSoup(page.content, 'html.parser')
 
 
 # Justifying the html to easily navigate through it
-
-# In[103]:
-
-
 #we can use prettify to print the html page as it is in justified form
 print(soup.prettify())
 
 
 # Function to download the video with properly naming the series
-
-# In[105]:
-
-
 #finding all the links to download the episodes in different seasons
 links = soup.find_all('a')
 links = [link.get('href') for link in links]
@@ -90,10 +58,6 @@ def videodownloader(urllist, seasn):
 
 
 # Looping thorugh different seasons of the tv series and calling videodownloader function
-
-# In[76]:
-
-
 # foor loop to call the video downloader function in loop to download all the video files.
 for link in links:
     #merging url to extend it
@@ -104,4 +68,3 @@ for link in links:
     urllist = [ url1 + link.get('href') for link in soup1.find_all('a') ]
     #calling videodownloader function to download the episodes one by one
     videodownloader(urllist,season)
-
